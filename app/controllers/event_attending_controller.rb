568fc8b05035e50c41964ee5fd71a1b3bf0d3b15
Cac <cacem@pm.me>
2025-08-29 19:26:32 -0400

    Build Attended Events Table

--- app/controllers/event_attending_controller.rb
+++ app/controllers/event_attending_controller.rb
@@ -5,5 +5,13 @@ class EventAttendingController < ApplicationController
 
   def create
     @event_attending = EventAttending.new
+    @event_attending[:attendee_id] = current_user.id
+    @event_attending[:attended_event_id] = params[:event_id]
+
+    if @event_attending.save!
+      redirect_to event_path(params[:event_id])
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
